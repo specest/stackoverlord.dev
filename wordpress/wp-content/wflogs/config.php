726718b0d1636e92ab626ebf882b92bf062b17b9
Peeter Kolk <kolk.peeter@gmail.com>
2023-11-14 07:00:03 +0000

    added wordfence

---
+++ wordpress/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1700203098;s:7:"authKey";s:64:"l!,5Q[7]gV3E`Vi{jn]y}]27W8{!?~1DA ANhLP%%Y9Rl<=[b3P<]0jDEfz%=y3{";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:57;}
